5955e47a0335cbecab19bf5dc625e9db1f416cca
jooheekim0713 <jooheekim.dev@gmail.com>
2022-07-09 15:47:50 +0900

    Use typescript, react-router-dom
    Create Coins.tsx to view coinlists and Coin.tsx to view coin details

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+import { theme } from './theme';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as Element);
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
